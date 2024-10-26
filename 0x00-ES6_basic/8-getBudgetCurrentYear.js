6c3425461d617ed487e9e14bce3bda20181f62d9
Freedom-21 <yenesewfreedom8@gmail.com>
2024-10-26 02:44:09 +0200

    all

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,8 @@
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+    return {
+      [`income-${getCurrentYear()}`]: income,
+      [`gdp-${getCurrentYear()}`]: gdp,
+      [`capita-${getCurrentYear()}`]: capita,
+    };
+  }
+  
